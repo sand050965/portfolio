6a562fb098a1c6237008b9ecd1b7bfc2b7fec525
Blake Yang <sand050965@gmail.com>
2026-05-13 23:21:00 -0700

    feat: Added hero component

---
+++ components/Hero.tsx
@@ -0,0 +1,46 @@
+import {Spotlight} from "@/components/ui/spotlight";
+import {cn} from "@/lib/utils";
+
+const Hero = () => {
+    return (
+        <div className="pb-20 pt-36">
+            <div>
+                <Spotlight
+                    className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+                    fill="white"
+                />
+                <Spotlight
+                    className="h-[80vh] w-[50vw] top-10 left-full"
+                    fill="purple"
+                />
+                <Spotlight
+                    className="top-28 left-80 h-[80vh] w-[50vw]"
+                    fill="blue"
+                />
+            </div>
+
+            <div className="relative flex h-screen w-full items-center justify-center bg-white dark:bg-black-100 absolute top-0 ">
+                <div
+                    className={cn(
+                        "absolute inset-0",
+                        "[background-size:40px_40px]",
+                        "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+                        "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]",
+                    )}
+                />
+                <div
+                    className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black-100"></div>
+
+                <div className="flex justify-center relative my-20 z-10">
+                    <div
+                        className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                        <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">Dynamic Web
+                            Magic with Next.js</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
