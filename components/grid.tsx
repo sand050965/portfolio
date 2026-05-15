accba073734c424b62965e86d2799929cccbc303
Blake Yang <sand050965@gmail.com>
2026-05-15 00:31:10 -0700

    feat: Added grid component

---
+++ components/grid.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BentoGrid } from "@/components//ui/bento-grid";
+
+const Grid = () => {
+    return (
+        <section id="about">
+            <BentoGrid></BentoGrid>
+        </section>
+    );
+};
+
+export default Grid;
