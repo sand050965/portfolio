a2f5150eb5c96cad1ecab8ee8feca4675deb88e6
Blake Yang <sand050965@gmail.com>
2026-05-18 23:10:48 -0700

    feat: Added work experience section

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import FloatingNav from "@/components/ui/floating-navbar";
 import Grid from "@/components/grid";
 import Project from "@/components/project";
 import Client from "@/components/client";
+import Experience from "@/components/experience";
 import { navItems } from "@/data";
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
                 <Grid/>
                 <Project/>
                 <Client/>
+                <Experience/>
             </div>
         </main>
     );
