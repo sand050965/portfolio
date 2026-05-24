69c8d01470afa8ce0ee67394c62d9b7be08953c3
Blake Yang <sand050965@gmail.com>
2026-05-23 22:41:59 -0700

    feat: Updated title for the projection section

--- components/project.tsx
+++ components/project.tsx
@@ -9,8 +9,8 @@ const RecentProjects = () => {
     return (
         <div className="py-20" id="projects">
             <h1 className="heading">
-                A small selection of{" "}
-                <span className="text-purple">recent projects</span>
+                Collection of{" "}
+                <span className="text-purple">Projects</span>
             </h1>
             <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
                 {projects.map(({
