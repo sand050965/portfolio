6dfc3dc66f3e3efc637a601f959ecac8a2e017db
Blake Yang <sand050965@gmail.com>
2026-09-04 19:01:10 -0700

    fix: Fixed a bug where navbar items would overlap when scrolling down

--- components/ui/resizable-navbar.tsx
+++ components/ui/resizable-navbar.tsx
@@ -101,7 +101,7 @@ export const NavBody = ({ children, className, visible }: NavBodyProps) => {
                 damping: 50,
             }}
             style={{
-                minWidth: "800px",
+                minWidth: "1000px",
             }}
             className={cn(
                 "relative z-[9999] mx-auto hidden w-full max-w-7xl flex-row items-center justify-between self-start rounded-full bg-transparent px-4 py-2 lg:flex dark:bg-transparent",
